e574b83a84f0b51b8f2c9545ef10d1ea783e07e4
Rae Wiechens <raeannw321@gmail.com>
2026-05-08 23:26:30 -0500

    created blog post cards and started blog feed section

---
+++ src/components/BlogFeed/BlogFeed.tsx
@@ -0,0 +1,35 @@
+import { Post } from "@/lib/sample-data";
+import BlogPostCard from "@/components/BlogPostCard/BlogPostCard";
+import styles from "./BlogFeed.module.css";
+
+interface BlogFeedProps {
+  posts: Post[];
+  cardsPerRow?: number;
+}
+
+function chunkArray<T>(arr: T[], size: number): T[][] {
+  return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+    arr.slice(i * size, i * size + size),
+  );
+}
+
+export default function BlogFeed({ posts, cardsPerRow = 2 }: BlogFeedProps) {
+  const rows = chunkArray(posts, cardsPerRow);
+
+  return (
+    <div className={styles.feed}>
+      {rows.map((rowPosts, i) => (
+        <div
+          key={i}
+          className={`${styles.row} ${
+            i % 2 === 0 ? styles.rowDark : styles.rowAccent
+          }`}
+        >
+          {rowPosts.map((post) => (
+            <BlogPostCard key={post.slug} post={post} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
