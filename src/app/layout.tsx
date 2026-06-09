559a54405bcfa287384fb9b882ab3508b74ea1ac
Rae Wiechens <raeannw321@gmail.com>
2026-06-09 06:13:43 -0500

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Alegreya, Rozha_One } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import Nav from "@/components/ui/Nav/Nav";
 import Footer from "@/components/ui/Footer/Footer";
 
@@ -37,6 +38,7 @@ export default function RootLayout({
         <Nav />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
